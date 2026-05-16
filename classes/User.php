4f10c2d6742797da24324a5ca19d6445891d6d44
rokaszumbys <rokas.zumbys@stud.svako.lt>
2026-05-16 15:01:28 +0300

    Pridėta User klasė registracijai ir prisijungimui

---
+++ classes/User.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once "Encryptor.php";
+
+class User {
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function register($username, $password) {
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+        $plainKey = bin2hex(random_bytes(16));
+
+        $encryptor = new Encryptor();
+        $encryptedKey = $encryptor->encrypt($plainKey, $password);
+
+        $sql = "INSERT INTO users (username, password_hash, encrypted_key) 
+                VALUES (:username, :password_hash, :encrypted_key)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([
+            ":username" => $username,
+            ":password_hash" => $passwordHash,
+            ":encrypted_key" => $encryptedKey
+        ]);
+    }
+
+    public function login($username, $password) {
+        $sql = "SELECT * FROM users WHERE username = :username";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([":username" => $username]);
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user["password_hash"])) {
+            return $user;
+        }
+
+        return false;
+    }
+}
