7b44b1f45f6181b92eace0641b29f1ed8b630e8d
rokaszumbys <rokas.zumbys@stud.svako.lt>
2026-05-16 13:39:13 +0300

    Pridėta Database klasė

---
+++ classes/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "password_project";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Prisijungimo klaida: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
