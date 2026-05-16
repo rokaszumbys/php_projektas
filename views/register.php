43d11bf81011eba738325d2ac0fe0489d29558fe
rokaszumbys <rokas.zumbys@stud.svako.lt>
2026-05-16 20:52:56 +0300

    Sukurtas registracijos puslapis

---
+++ views/register.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "../classes/Database.php";
+require_once "../classes/User.php";
+
+$message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $database = new Database();
+    $db = $database->connect();
+
+    $user = new User($db);
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if ($user->register($username, $password)) {
+        $message = "Registracija sėkminga!";
+    } else {
+        $message = "Registracijos klaida.";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Registracija</title>
+</head>
+<body>
+
+<h2>Registracija</h2>
+
+<p><?php echo $message; ?></p>
+
+<form method="POST">
+    <label>Vartotojo vardas:</label><br>
+    <input type="text" name="username" required><br><br>
+
+    <label>Slaptažodis:</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">Registruotis</button>
+</form>
+
+<br>
+<a href="login.php">Jau turi paskyrą? Prisijunk</a>
+
+</body>
+</html>
